7aa27d4c2a3716a41dc0219de5b196b8455eaa3b
Moe Aboulkheir <moe.aboulkheir@gmail.com>
2022-12-28 08:43:04 +0000

    This works OK

--- src/cromulator/browser.cljs
+++ src/cromulator/browser.cljs
@@ -20,8 +20,16 @@
                :n2 [1     80]
                :n3 [1     80]}))
 
-(def state (r/atom {:range     bounds
-                    :path-refs [nil nil]}))
+(defn- initial-inputs []
+  (into {}
+    (for [[k range'] bounds]
+      [k (apply uniform k range')])))
+
+(def state (r/atom {:inputs    [(initial-inputs) (initial-inputs)]
+                    :range     bounds
+                    :path-refs [nil nil]
+                    :tweening? false
+                    :iter      0}))
 
 (defn- event-value [e]
   (-> e .-target .-value))
@@ -43,45 +51,69 @@
                    :on-change #(swap! state assoc-in [:range v 1] (event-value %))}]
           [:span {:style {:width "2em" :display "inline-block" :text-align "right"}} hi]])])))
 
-(defn pt [x]
-  (println x)
-  x)
-
 (defn- superformula-inputs []
   (into {}
     (for [[k range'] (:range @state)]
       [k (apply uniform k range')])))
 
-(defn superformula-svg [{path-id :path-id path-atom :path-atom}]
-  (let [values    (superformula-inputs)
-        pts       (geom/superformula-points [0 0] values)
+(let [W 100
+      H 100]
+  (defn superformula-path [& [inputs]]
+    (let [inputs (or inputs (superformula-inputs))]
+      (let [pts   (geom/superformula-points [0 0] inputs)
         pth       (pc/roundPath (pc/normalizePath (clj->js (partition 3 (spline/pts->path pts)))) 6)
         {w :width
          h :height
          x :x
          y :y}     (js->clj (pc/getPathBBox pth) :keywordize-keys true)
-        scale      (cond-> (min (/ 100 w) (/ 100 h)) (or (< 100 w) (< 100 h)) -)]
+        scale      (cond-> (min (/ W w) (/ H h)) (or (< W w) (< H h)) -)
+        trans-x    (min 50 (abs (- (/ (- W w) 2) x)))
+        trans-y    (min 50 (abs (- (/ (- H h) 2) y)))]
+    (-> pth
+        (pc/transformPath #js {:translate #js [trans-x trans-y]
+                               :scale     scale})
+        pc/pathToString)))))
+
+(defn superformula-svg [{[id0 id1] :path-ids [a0 a1] :path-atoms [in0 in1] :inputs}]
+  (let [p0 (superformula-path)]
     (fn []
       [:svg {:viewBox "0 0 100 100"}
-       [:path {:ref (partial reset! path-atom)
-               :transform (str "translate(" (min 50 (abs (- (/ (- 100 w) 2) x))) " " (min 50 (abs (- (/ (- 100 h) 2) y)))
-                               ") scale(" scale ")")  :id path-id :d (pc/pathToString pth)}]])))
+       [:path {:ref (partial reset! a0)
+               :id  id0
+               :d   p0}]
+       [:path {:style {:visibility "hidden"}
+               :ref (partial reset! a1)
+               :id  id1
+               :d   (superformula-path @in1)}]])))
 
-(defn tweener [n1 n2]
-  (when (not-any? nil? (:path-refs @state))
-    (r/with-let [tween (kute/to (str "#" n1) #js {:path (str "#" n2)} #js {:yoyo true})]
-      (when-not (.-playing tween)
-        (.start tween))
-      [:<>])))
+(defn tweener [n1 p0 n2 p1 t? cb]
+  (when (and @p0 @p1 (not @t?))
+    (r/with-let [tween (kute/to (str "#" n1)
+                                #js {:path (str "#" n2)}
+                                #js {:duration   1000
+                                     :easing     "easingCubicOut"
+                                     :onComplete (fn []
+                                                   (reset! t? false)
+                                                   (cb))})]
+      (reset! t? true)
+      (.start tween)))
+  [:<>])
 
 (defn home-page []
-  [:div
-   [sliders]
-   [superformula-svg {:path-id "visible" :path-atom (r/cursor state [:path-refs 0])}]
-   [:div {:style {:display "none"}}
-    [superformula-svg {:path-id "invisible" :path-atom (r/cursor state [:path-refs 1])}]]
-   [tweener "visible" "invisible"]
-   ])
+  (r/with-let [iter (r/cursor state [:iter])
+               p0   (r/cursor state [:path-refs 0])
+               p1   (r/cursor state [:path-refs 1])
+               in0  (r/cursor state [:inputs 0])
+               in1  (r/cursor state [:inputs 1])
+               t?   (r/cursor state [:tweening?])]
+
+    [:div
+     [sliders]
+     [superformula-svg {:path-ids ["visible" "invisible"] :path-atoms [p0 p1] :inputs [in0 in1]}]
+     [tweener "visible" p0 "invisible" p1 t? (fn []
+                                               (let [v (swap! state update :iter inc)]
+                                                 (when (pos? (mod (:iter v) 2))
+                                                   (reset! in1 (superformula-inputs)))))]]))
 
 
 (defn mount-root []
--- src/cromulator/geom.cljc
+++ src/cromulator/geom.cljc
@@ -3,22 +3,17 @@
              :refer [pointwise both TAU cos sin pow rad]]))
 
 (defn- sf-point [m n1 n2 n3 a b phi]
-  (let [t1 (-> m (* phi) (/ 4) cos (/ a) abs (pow n2))
-        t2 (-> m (* phi) (/ 4) sin (/ b) abs (pow n3))
-        r  (pow (+ t1 t2) (/ 1 n1))]
+  (let [xf (fn [f exp] (-> m (* phi) (/ 4) f (/ a) abs (pow exp)))
+        r  (pow (+ (xf cos n2) (xf sin n3)) (/ 1 n1))]
     (if (zero? r)
       [0 0]
       (both * (/ 1 r) (cos phi) (sin phi)))))
 
-(defn superformula-points
-  ([coords m n1 n2 n3 a b & [{res :resolution :or {res 256}}]]
-   (into [coords]
-     (comp
-      (map (partial * (/ TAU res)))
-      (map (partial sf-point m n1 n2 n3 a b))
-      (map (partial pointwise + coords)))
-     (range (inc res))))
-  ([coords m n1 n2 n3]
-   (superformula-points coords m n1 n2 n3 1 1))
-  ([coords {m :m n1 :n1 n2 :n2 n3 :n3 a :a b :b}]
-   (superformula-points coords m n1 n2 n3 a b)))
+(defn superformula-points [coords {m :m n1 :n1 n2 :n2 n3 :n3 a :a b :b}
+                           & [{res :resolution :or {res 720}}]]
+  (into [coords]
+    (comp
+     (map (partial * (/ TAU res)))
+     (map (partial sf-point m n1 n2 n3 a b))
+     (map (partial pointwise + coords)))
+    (range (inc res))))
